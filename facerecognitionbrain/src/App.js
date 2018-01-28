2986d98407ecf10bf23e5ec71eb072b11cbff7b3
Mattemyo <matias97marti@gmail.com>
2018-01-28 19:12:04 +0100

    add signin form component

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -3,6 +3,7 @@ import Particles from "react-particles-js";
 import Clarifai from "clarifai";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Navigation from "./components/Navigation/Navigation";
+import Signin from "./components/Signin/Signin";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
@@ -76,6 +77,7 @@ class App extends Component {
         {/* Background */}
         <Particles className="particles" params={particlesOptions} />
         <Navigation />
+        <Signin />
         <Logo />
         <Rank />
         <ImageLinkForm
